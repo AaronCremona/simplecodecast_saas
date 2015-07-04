3008f6ef37213ad1e447160716f38cb8dc9e8ded
Aaron Cremona <dubbacremona@gmail.com>
2015-07-04 07:03:07 +0000

    fixed syntax

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'dubbacremona@gmail.com'
+  default :to => 'dubbacremona@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
