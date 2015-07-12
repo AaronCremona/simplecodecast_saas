f08bea9bc2bcc2204c8829b5225d8b1057cdec23
Aaron Cremona <dubbacremona@gmail.com>
2015-07-12 22:17:02 +0000

    Added notice for wrong sign up url and styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+before_filter :select_plan, only: :new
+  
   def create
     super do |resource|
       if params[:plan]
@@ -7,8 +9,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
           resource.save_with_payment
         else
           resource.save
-        end
-      end
-    end
-  end
-end
+        end #if
+      end #if 
+    end #super
+  end #create
+  
+  private
+  def select_plan
+    unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+      flash[:notice] = "Please select a memebership plan to sign up."
+      redirect_to root_url
+    end #unless
+  end  #select plan
+  
+end #class
