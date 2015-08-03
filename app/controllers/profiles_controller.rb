8d6742521bfe608862ab41ca0ce9d3e627b884df
Aaron Cremona <dubbacremona@gmail.com>
2015-08-03 00:17:49 +0000

    Added bare bones user show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,8 +4,8 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile
   end
   
-  def create
-    @user = User.find( params[:user_id])
+def create 
+    @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
     if @profile.save
       flash[:success] = "Profile Updated!"
@@ -14,7 +14,6 @@ class ProfilesController < ApplicationController
       render action: :new
     end
   end
-  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
